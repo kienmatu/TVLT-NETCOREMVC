3484db86c5cf5bceef6e68dd192ef96248401256
kiendt <kienmatu1@gmail.com>
2019-07-13 12:34:15 +0700

    ADD Sess4.sln

---
+++ Sess4/Sess4/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sess4.Models;
+
+namespace Sess4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student objStudent = new Student();
+            
+            objStudent.StudentName = "KIÊN ĐINH KUTE";
+            objStudent.StudentAge = 20;
+            objStudent = null;
+
+            return View(objStudent);
+            //ctrl + click
+        }
+    }
+}
---
+++ Sess4/Sess4/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sess4.Models
+{
+    public class Student
+    {
+       public Student()
+        {
+            this.StudentName = "default";
+        }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+    }
+}
